533496ca00194b19cc4cdc799548083f5af9bb7f
berkayzaimdev <berkayzaimdev@gmail.com>
2024-07-24 12:01:33 +0300

    Added Publish/Subscribe template

---
+++ src/MessageTemplates/PublishSubscribe/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://xncluoek:Pg5sa3Y0HAPsVkv0ox0b6y8qi1zObKaD@moose.rmq.cloudamqp.com/xncluoek");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+string exchangeName = "example-pub-sub-exchange";
+
+channel.ExchangeDeclare(
+    exchange: exchangeName,
+    type: ExchangeType.Fanout // pub-sub davranışına uygun olarak, fanout exchange biçilmiş kaftandır. abone olan tüm kuyruklara mesaj gönderir
+    );
+
+string queueName = channel.QueueDeclare().QueueName;
+channel.QueueBind(
+    queue: queueName,
+    exchange: exchangeName,
+    routingKey: string.Empty
+    );
+
+EventingBasicConsumer consumer = new(channel);
+
+channel.BasicConsume(
+    queue: queueName,
+    consumer: consumer,
+    autoAck: false
+    );
+
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
---
+++ src/MessageTemplates/PublishSubscribe/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://xncluoek:Pg5sa3Y0HAPsVkv0ox0b6y8qi1zObKaD@moose.rmq.cloudamqp.com/xncluoek");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+string exchangeName = "example-pub-sub-exchange";
+
+channel.ExchangeDeclare(
+    exchange: exchangeName,
+    type: ExchangeType.Fanout // pub-sub davranışına uygun olarak, fanout exchange biçilmiş kaftandır. abone olan tüm kuyruklara mesaj gönderir
+    );
+
+for (int i = 0; i < 100; i++)
+{
+    byte[] message = Encoding.UTF8.GetBytes($"merhaba {i}");
+
+    channel.BasicPublish(
+        exchange: exchangeName,
+        routingKey: string.Empty,
+        body: message
+        );
+}
+
