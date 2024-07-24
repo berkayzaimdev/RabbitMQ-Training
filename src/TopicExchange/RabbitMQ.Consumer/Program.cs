cba96706e35acb408cde04efe289eebb04c59b63
berkayzaimdev <berkayzaimdev@gmail.com>
2024-07-24 10:55:42 +0300

    Added Topic Exchange

---
+++ src/TopicExchange/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://xncluoek:Pg5sa3Y0HAPsVkv0ox0b6y8qi1zObKaD@moose.rmq.cloudamqp.com/xncluoek");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "topic-exchange-example",
+    type: ExchangeType.Topic); // buralar klasik artık, publisher'ın aynısı olmalı
+
+Console.Write("Dinlenecek topic'i belirtiniz: ");
+string topic = Console.ReadLine();
+string queueName = channel.QueueDeclare().QueueName; // detayları çok önemli değil şuan
+
+channel.QueueBind(
+    queue: queueName,
+    exchange: "topic-exchange-example",
+    routingKey: topic); // topic'e göre mesajları dağıtır
+
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+
---
+++ src/TopicExchange/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://xncluoek:Pg5sa3Y0HAPsVkv0ox0b6y8qi1zObKaD@moose.rmq.cloudamqp.com/xncluoek");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "topic-exchange-example",
+    type: ExchangeType.Topic);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba {i + 1}");
+
+    Console.Write("Mesajın gönderileceği topic formatını belirtiniz: ");
+    string topic = Console.ReadLine();
+
+    channel.BasicPublish(
+        exchange: "topic-exchange-example",
+        routingKey: topic, // hangi topic'e göre mesaj göndereceğiz? burada belirliyoruz
+        body: message);
+}
