9eceafb1c005a7a4cf06a309633611a05c0b5a13
berkayzaimdev <berkayzaimdev@gmail.com>
2024-07-24 11:47:17 +0300

    Added P2P template

---
+++ src/MessageTemplates/P2P/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://xncluoek:Pg5sa3Y0HAPsVkv0ox0b6y8qi1zObKaD@moose.rmq.cloudamqp.com/xncluoek");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+string queueName = "example-p2p-queue";
+
+channel.QueueDeclare(
+    queue: queueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false
+    );
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: false,
+    consumer: consumer
+    );
+// tek bir consumer, tek bir queue'dan mesaj tüketecek
+
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
---
+++ src/MessageTemplates/P2P/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://xncluoek:Pg5sa3Y0HAPsVkv0ox0b6y8qi1zObKaD@moose.rmq.cloudamqp.com/xncluoek");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+string queueName = "example-p2p-queue";
+
+channel.QueueDeclare(
+    queue: queueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false
+    ); // tek bir queue'ya göndeririz
+
+byte[] message = Encoding.UTF8.GetBytes("merhaba");
+
+channel.BasicPublish( // exchange'e gerek yoktur.
+    exchange: string.Empty,
+    routingKey: queueName,
+    body: message
+    );
