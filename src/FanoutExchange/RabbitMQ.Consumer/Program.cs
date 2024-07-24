fecc7edea9277c60fb5ce45ca8137f04ccaf7ff6
berkayzaimdev <berkayzaimdev@gmail.com>
2024-07-24 10:36:41 +0300

    Added Fanout Exchange

---
+++ src/FanoutExchange/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://xncluoek:Pg5sa3Y0HAPsVkv0ox0b6y8qi1zObKaD@moose.rmq.cloudamqp.com/xncluoek");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "fanout-exchange-example",
+    type: ExchangeType.Fanout);
+
+Console.Write("Kuyruk adını giriniz: ");
+string _queueName = Console.ReadLine();
+
+channel.QueueDeclare(
+    queue: _queueName, 
+    exclusive: false // bu kuyruğa başka bağlantılar olabilir o yüzden false
+    );
+
+channel.QueueBind( // bir exchange ile kuyrukların ilişkilendirilmesi, bind işlemi
+    queue: _queueName,
+    exchange: "fanout-exchange-example",
+    routingKey: string.Empty // fanout exchange olduğu için routing key boş olmalı
+    );
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: _queueName,
+    autoAck: true,
+    consumer: consumer);
+
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
---
+++ src/FanoutExchange/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://xncluoek:Pg5sa3Y0HAPsVkv0ox0b6y8qi1zObKaD@moose.rmq.cloudamqp.com/xncluoek");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "fanout-exchange-example", 
+    type: ExchangeType.Fanout);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba {i+1}");
+
+    channel.BasicPublish(
+        exchange: "fanout-exchange-example", 
+        routingKey: string.Empty, // tüm kuyruklara mesaj iletececeği için routing key boş olmalıdır
+        body: message);
+}
